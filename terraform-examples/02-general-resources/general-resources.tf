95e63d6b870a19262e98dd8e8e7e56209e8da579
Adron Hall <adronhall@gmail.com>
2018-01-28 16:01:07 -0800

    Aight. working.

---
+++ terraform-examples/02-general-resources/general-resources.tf
@@ -0,0 +1,13 @@
+resource "google_compute_network" "ou_rdevelopment_network" {
+  name                    = "devnetwork"
+  auto_create_subnetworks = "true"
+}
+
+resource "aws_vpc" "new_vpc" {
+    name = "newvpcnetworkisnew"
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    tags {
+        Name = "terraform-aws-vpc"
+    }
+}
--- terraform-examples/02-static-ip/static-ip-addresses.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_network" "our_development_network" {
-  name                    = "our_gcp_development_resource_network"
-  auto_create_subnetworks = "true"
-}
-
